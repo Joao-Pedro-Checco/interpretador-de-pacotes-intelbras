38fd0d724efc67d37607180eda7a4468560bb00b
João Pedro Checco <joao.checco@domain.fulltime>
2023-12-19 11:15:04 -0300

    método conversor de hexadecimal para ascii

--- src/main/java/br/com/fulltime/fullarm/utils/FormatadorHexStr.java
+++ src/main/java/br/com/fulltime/fullarm/utils/FormatadorHexStr.java
@@ -14,4 +14,14 @@ public class FormatadorHexStr {
 
         return stringBuilder.toString().trim().toUpperCase();
     }
+
+    public static String hexParaAscii(String hexString) {
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < hexString.length(); i += 2) {
+            String str = hexString.substring(i, i + 2);
+            output.append((char)Integer.parseInt(str, 16));
+        }
+
+        return output.toString();
+    }
 }
