9623388de980b36ae6747664a78c97dd3add291a
João Pedro Checco <joao.checco@domain.fulltime>
2023-12-19 11:13:27 -0300

    injeção de dependências no parser de pacote

--- src/main/java/br/com/fulltime/fullarm/Main.java
+++ src/main/java/br/com/fulltime/fullarm/Main.java
@@ -1,5 +1,6 @@
 package br.com.fulltime.fullarm;
 
+import br.com.fulltime.fullarm.factory.PacoteFactory;
 import br.com.fulltime.fullarm.pacote.Pacote;
 import br.com.fulltime.fullarm.pacote.PacoteParser;
 import br.com.fulltime.fullarm.processador.ProcessadorPacote;
@@ -13,7 +14,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String hexString = FormatadorHexStr.formatar(scanner.nextLine());
         ValidadorHexString.validar(hexString);
-        ProcessadorPacote processador = new PacoteParser().identificarPacote(hexString);
+        ProcessadorPacote processador = new PacoteParser(new PacoteFactory()).identificarPacote(hexString);
 
         Pacote pacote = processador.processar(hexString);
         System.out.println(pacote);
--- src/main/java/br/com/fulltime/fullarm/pacote/PacoteParser.java
+++ src/main/java/br/com/fulltime/fullarm/pacote/PacoteParser.java
@@ -4,16 +4,18 @@ import br.com.fulltime.fullarm.factory.PacoteFactory;
 import br.com.fulltime.fullarm.processador.ProcessadorPacote;
 
 public class PacoteParser {
+    private PacoteFactory factory;
 
-    public PacoteParser() {
+    public PacoteParser(PacoteFactory factory) {
+        this.factory = factory;
     }
 
     public ProcessadorPacote identificarPacote(String hexString) {
         String[] bytes = hexString.split(" ");
 
         if (ehFrameLongo(bytes))
-            return new PacoteFactory().buscarProcessador(bytes[1]);
-        return new PacoteFactory().buscarProcessador(bytes[0]);
+            return factory.buscarProcessador(bytes[1]);
+        return factory.buscarProcessador(bytes[0]);
     }
 
     private boolean ehFrameLongo(String[] bytes) {
